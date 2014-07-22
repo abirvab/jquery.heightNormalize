51b81b84c4736cdc6eca930fcaba63c8f77f92f1
abirvab <Abirvab@gmail.com>
2014-07-22 14:57:52 +0530

    Update jquery.heightNormalize.js

--- jquery.heightNormalize.js
+++ jquery.heightNormalize.js
@@ -4,39 +4,44 @@
 
     $.fn.heightNormalize = function (options) {
         var settings = $.extend({
-            addToHeight: 0            
+            addToHeight: 0
         }, options);
-
+        var oldIE = $("html").is("ie8") || $("html").is("ie7") || $("html").is("lt-ie7");
         var heights = new Array(this.length);
         if (this.length > 1) {
-		$.each(this, function (idx, item) {
-            var height = 0;
-            if (oldIE) {
+            $.each(this, function (idx, item) {
+                var height = 0;
+                var childrens = $(item).children();
+                if (childrens.length != 0) {
+                    if (oldIE) {
+                        height = $(item).height();
+                    } else {
+                        $.each(childrens, function (id, child) {
+                            if ($(child).css("display") !== "none")
+                                height += $(child).height();
+                        });
+                        $(item).height(height);
+                    }
+                } else {
                     height = $(item).height();
-            } else {
-                    $.each($(item).children(), function (id, child) {
-                    if($(child).css("display")!="none")
-                    height += $(child).height();
-                });
-                $(item).height(height);
-            }
+                }
 
-            heights[idx] = height;
-        });
-        var maxheight = 0;
-        $.each(heights, function (id, height) {
-            if (height >= maxheight) {
-                maxheight = height;
-            }
-        });
-        $.each(this, function (idx, item) {
-            $(item).height(maxheight+settings.addToHeight);
+                heights[idx] = height;
+            });
+            var maxheight = 0;
+            $.each(heights, function (id, height) {
+                if (height >= maxheight) {
+                    maxheight = height;
+                }
+            });
+            $.each(this, function (idx, item) {
+                $(item).height(maxheight + settings.addToHeight);
 
-        });
-        contentAreaHeight = maxheight;
-	}
+            });
+            contentAreaHeight = maxheight;
+        }
         return this;
 
     };
 
-} (jQuery));
+}(jQuery));
